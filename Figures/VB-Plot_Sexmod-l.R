0f2bb6bfb2512b1dc0a2bb93fa43c1f29de87784
Alex Benecke <abenecke@protonmail.com>
2018-01-31 10:24:46 -0500

    Figures for Talk

--- Figures/VB-Plot_Sexmod-l.R
+++ Figures/VB-Plot_Sexmod-l.R
@@ -1,4 +1,4 @@
-
+### Load Packages and Data 
 library(FSA)
 library(tidyverse)
 library(nlme)
@@ -6,20 +6,20 @@ library(nlme)
 LMBL <- read.csv("Data/Clean-Data/2016_largemouth-bass_long-format.csv") %>%
   arrange(FID,Agei)
 
+tmp.male <- read.csv("Data/Clean-Data/tmp.male.csv")
+headtail(tmp.male)
+
+tmp.female <- read.csv("Data/Clean-Data/tmp.female.csv")
+headtail(tmp.female)
+
+load("model-output/sexmod.l.rda")
+
 ### Making factors factors
 LMBL$FID <- factor(LMBL$FID)
 LMBL$Site <- factor(LMBL$Site)
 LMBL$SEXCON <- factor(LMBL$SEXCON)
 LMBL$Sex <- factor(LMBL$Sex)
 
-LVB <- function(x, Linf, K, t0){
-  y = Linf * (1 - exp(-K * (x - t0)))
-  y
-}
-LVB <- vbFuns()
-
-LVB(5, 422.8, 0.39, -0.40)
-LVB(5, Linf = c(422.8, 0.39, -0.40)) ### Should be the same output
 
 ### Just looking at data
 head(LMBL)
@@ -39,46 +39,61 @@ LMBL <- filterD(LMBL,!is.na(FID))
 str(LMBL)
 headtail(LMBL)
 
-ddatgr = groupedData(BI.len ~ Agei|FID, data = LMBL,
+datgr = groupedData(BI.len ~ Agei|FID, data = LMBL,
                     labels = list(x = "Age", y = "Size"),
                     units = list(x = "(Years)", y = "(mm)"))
-load("model-output/sexmod.l.rda")
 
-#### Prepare plot
+### Create VB Function
+LVB <- function(x, Linf, K, t0){
+  y = Linf * (1 - exp(-K * (x - t0)))
+  y
+}
+LVB <- vbFuns()
+
+LVB(5, 422.8, 0.39, -0.40)
+LVB(5, Linf = c(422.8, 0.39, -0.40)) ### Should be the same output
+
+
+#### Prepare Plot #####################################
 Axes <- seq(100,450,by=50)
 Years <- seq(0,10,by=1)
 
-## plot individual fish data
-## plot the fixed parameter model
-## plot individual fish models
-
-### rgb(0,1,0,0.25, maxColorValue=1)
-
-par(mar=c(3.25,3.25,0,0),mgp=c(2.1,0.4,0),tcl=-0.2,
+par(mar=c(3.25,4.5,0,0),mgp=c(2.1,0.4,0),tcl=-0.2,
     lwd=2,
     cex=1.25,
-    cex.lab = 1,
-    cex.axis = 1)
+    cex.lab = 1.5,
+    cex.axis = 1.5)
 
-### plot sexmod.l
+### Plot sexmod.l ##############################################
+
 plot(jitter(LMBL$Agei),LMBL$BI.len,
-     col=ifelse(LMBL$Sex==1,rgb(0,0,1,0.25, maxColorValue=1),rgb(1,0,0,0.25, maxColorValue=1)),
+     col=ifelse(LMBL$Sex==1,rgb(0,0,1,0.4, maxColorValue=1),rgb(1,0,0,0.4, maxColorValue=1)),
      pch=19,
      ylim=c(100,500),
      xlim=c(0,10),
-     xlab = "Age (Years)",
-     ylab = "Back-Calculated Length (mm)",
+     xlab = "",
+     ylab = "",
      bty="n",
      yaxt="n",
      xaxt="n")
 
 axis(2,at = Axes, las=2, lwd=2)
+mtext("Total Length (mm)",
+      side=2,
+      line=3,
+      cex=2) 
+
 axis(1,at = Years, las=1,lwd=2)
+mtext("Age (Years)",
+      side = 1,
+      line=2,
+      cex=2)
 
-abline(h=425.4478748,lty=2,col="blue") ### Males
-abline(h=439.0623179,lty=2,col="red") ### females
+abline(h=425.4478748,lty=2,col="blue",lwd=2) ### Males
+abline(h=439.0623179,lty=2,col="red",lwd=2) ### Females
+abline(h=457, lty=4, col="green", lwd = 2) ### Angler Satisfaction
 
-x <- seq(1,11,by=1)
+x <- seq(0,11,by=1)
 lines(x, fixef(sexmod.l)[1] * (1 - exp(-fixef(sexmod.l)[3] * (x - (fixef(sexmod.l)[4])))),
       lwd=3, 
       col="blue") ### Males
@@ -86,37 +101,32 @@ lines(x, fixef(sexmod.l)[1] * (1 - exp(-fixef(sexmod.l)[3] * (x - (fixef(sexmod.
 lines(x, fixef(sexmod.l)[2] * (1 - exp(-fixef(sexmod.l)[3] * (x - (fixef(sexmod.l)[4])))),
       lwd=3, 
       col="red") ### Females
-### r Males
-#length(unique(LMBL$FID[LMBL$Sex==1])) ### n = 56 males
 
 for(i in 1:56){
-  lines(x, (coef(sexmod.l)[i,1] + coef(sexmod.l)[i,5]) * (1 - exp(- coef(sexmod.l)[i,3]   
-                                                                  * (x - ( coef(sexmod.l)[i,4] )))),lwd=3,col=rgb(0,0,1,0.1),
+  lines(x, (tmp.male[i,2] + tmp.male[i,6]) * (1 - exp(- tmp.male[i,4] * (x - (tmp.male[i,5])))),
+        lwd=3,
+        col=rgb(0,0,1,0.15),
         lty=3) } ### Males
 
-
-### Females
-#length(unique(LMBL$FID[LMBL$Sex==2])) ### n = 69 females
-
 for(i in 1:69){
-  lines(x, (coef(sexmod.l)[i,2] + coef(sexmod.l)[i,5]) * (1 - exp(- coef(sexmod.l)[i,3]   
-                                                                  * (x - ( coef(sexmod.l)[i,4] )))),lwd=3,col=rgb(1,0,0,0.1),
+  lines(x, (tmp.female[i,3] + tmp.female[i,6]) * (1 - exp(- tmp.female[i,4] * (x - (tmp.female[i,5])))),
+        lwd=3,
+        col=rgb(0.85,0,0,0.15),
         lty=3) } ### Females
 
-### Legend
-legend("topleft",
-       legend = print(expression(L[i]==425 %*% (1 - e **{-0.39 %*% (t[i] + 0.39)}))),
+legend(x=-1,y=440,
+       legend = print(expression(L[t]==425 %*% (1 - e **{-0.39 %*% (t + 0.39)}))),
        bty="n",
        cex=1.5,
        text.col = "blue") ### VB Equation Males
 
-legend("topright",
-       legend = print(expression(L[i]==439 %*% (1 - e **{-0.39 %*% (t[i] + 0.39)}))),
+legend(x=-1,y=515,
+       legend = print(expression(L[t]==439 %*% (1 - e **{-0.39 %*% (t + 0.39)}))),
        bty="n",
        cex=1.5,
-       text.col = "red") ### VB Equation Females
+       text.col = rgb(0.85,0,0)) ### VB Equation Females
 
 legend("bottomright",
        legend = c("Male", "Female"),
-       col=c("blue", "red"),
+       col=c(rgb(0,0,1), rgb(0.85,0,0)),
        lty=1, pch=19, bty="n", cex=1.5)
